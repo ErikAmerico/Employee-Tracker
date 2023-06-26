2f8726581d4a633cf5c22757cacc737fb62be3af
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-25 21:05:53 -0400

    updated package.json. removed type:module.

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-  department_id INT NOT NULL PRIMARY KEY,
+  department_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    role_id INT NOT NULL PRIMARY KEY,
+    role_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-    employee_id INT NOT NULL PRIMARY KEY,
+    employee_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
